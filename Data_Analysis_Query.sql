03c20cd0e5753919878e25a03fa99b1ed074d6c0
jalenabellard-official <jalen.abellard@gmail.com>
2026-08-18 18:53:15 -0400

    More Data Analysis

--- Data_Analysis_Query.sql
+++ Data_Analysis_Query.sql
@@ -5,18 +5,31 @@
 
 
 
--- Start of data analysis--
+-----------------------------------------Start of data analysis------------------------------------------------------
+
+--Query 1
+
+-- Order of industries by most layoffs from 2020 through 2026--
+-- SELECT
+-- industry,
+-- COUNT(DISTINCT company) AS num_of_companies,
+-- CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
+-- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
+-- FROM layoffs_clean_test
+-- GROUP BY industry
+-- ORDER BY total_laid_off DESC
 
 
+-----------------------------------------------------------------------------------------
 
--- Order industries by most layoffs--
-SELECT
+
+--Order of industries and layoffs from 2020 through 2026 per year
+SELECT 
 industry,
 COUNT(DISTINCT company) AS num_of_companies,
 CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
-CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off,
+YEAR(date) AS Year
 FROM layoffs_clean_test
-GROUP BY industry
-ORDER BY total_laid_off DESC
-
-
+GROUP BY YEAR(date), industry
+ORDER BY industry, YEAR(date) ASC
--- Data_Exploration.sql
+++ Data_Exploration.sql
@@ -3,7 +3,9 @@
 -- cleaned: pandas
 -- goal: data exploration and investigation
 
----------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+
+--Query 1 Exploration
 
 --Noticed that Hardware had the 3rd most layoffs at 106k despite only having 38 companies
 --Results: A query and found Dell and HP made up 60% with 43k and 23k respectively 
@@ -18,5 +20,39 @@
 -- ORDER BY SUM(total_laid_off) DESC
 
 
---------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+--Query 2 Exploration
+
+-- Noticed: That when layoff percentages where averaged, some years of industries had a 100 percent 
+-- Results: Over the 6 years of the avalibale data, 349 companies had a 100 percent layoff percentage, which almost certianly iducated 349 shutdown comapnies
+
+-- SELECT 
+-- DISTINCT company,
+-- percentage_laid_off,
+-- industry
+-- FROM layoffs_clean_test
+-- WHERE percentage_laid_off > 0.99
+
+
+
+--A check to see how many companies shut down per year, 2023 is the clear winner at 105 with the second closet being 2024 at 85
+-- SELECT
+-- YEAR(date),
+-- COUNT(DISTINCT company) AS total_layoffs
+-- FROM layoffs_clean_test
+-- WHERE percentage_laid_off > 0.99
+-- GROUP BY YEAR(date)
+-- ORDER BY YEAR(date)
+
+
+--A check to see how the number of shutdown companies per industry per year 
+SELECT
+industry,
+COUNT( DISTINCT company) AS company_count,
+YEAR(date) AS year
+FROM layoffs_clean_test
+WHERE percentage_laid_off > 0.99
+GROUP BY industry, YEAR(date)
+ORDER BY industry
 
