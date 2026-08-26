c7fd28fea53be7e339d5d951422533b3cde4054a
jalenabellard-official <jalen.abellard@gmail.com>
2026-08-26 19:19:39 -0400

    Final Checks
    uncomments all Queries, fixed typos and path errors, changed data cleaning from a plain text to a .py

--- Data_Analysis_Query.sql
+++ Data_Analysis_Query.sql
@@ -1,7 +1,9 @@
 -- table name: layoffs_clean_test
--- source: layoffs_clean.csv
+-- source: data/layoffs_clean.csv
 -- cleaned: pandas
 -- goal: analyze layoffs
+
+-- run queries individually by highlighting and executing
 
 
 
@@ -10,15 +12,15 @@
 --Query Group 1
 
 -- Order of industries by most layoffs from 2020 through 2026--
--- SELECT
--- industry,
--- COUNT(DISTINCT company) AS num_of_companies,
--- CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
--- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
--- FROM layoffs_clean_test
--- WHERE industry != 'Other'
--- GROUP BY industry
--- ORDER BY total_laid_off DESC
+SELECT
+industry,
+COUNT(DISTINCT company) AS num_of_companies,
+CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
+FROM layoffs_clean_test
+WHERE industry != 'Other'
+GROUP BY industry
+ORDER BY total_laid_off DESC;
 
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -26,45 +28,45 @@
 --Query Group 2
 
 --Order of industries and layoffs from 2020 through 2026 per year, including companies that shutdown (had a 100 percent layoff)
--- SELECT 
--- industry,
--- COUNT(DISTINCT company) AS num_of_companies,
--- CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
--- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off,
--- YEAR(date) AS Year
--- FROM layoffs_clean_test
--- WHERE industry != 'Other'
--- GROUP BY YEAR(date), industry
--- ORDER BY industry, YEAR(date) ASC
+SELECT 
+industry,
+COUNT(DISTINCT company) AS num_of_companies,
+CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off,
+YEAR(date) AS Year
+FROM layoffs_clean_test
+WHERE industry != 'Other'
+GROUP BY YEAR(date), industry
+ORDER BY industry, YEAR(date) ASC;
 
 
 --All of the industries and their shutdown numbers from 2020 through 2026
--- SELECT
--- industry,
--- COUNT( DISTINCT company) AS company_count,
--- YEAR(date) AS year
--- FROM layoffs_clean_test
--- WHERE percentage_laid_off > 0.99 AND industry != 'Other'
--- GROUP BY industry, YEAR(date)
--- ORDER BY industry
+SELECT
+industry,
+COUNT( DISTINCT company) AS company_count,
+YEAR(date) AS year
+FROM layoffs_clean_test
+WHERE percentage_laid_off > 0.99 AND industry != 'Other'
+GROUP BY industry, YEAR(date)
+ORDER BY industry;
 
 --All of the industry layoffs where more than five companies reported layoffs, excluding companies that shutdown 
--- WITH industry_year_breakdown AS (
--- SELECT 
--- industry,
--- COUNT(DISTINCT company) AS num_of_companies,
--- CAST(SUM(total_laid_off )AS INT) AS total_laid_off,
--- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off,
--- YEAR(date) AS Year
--- FROM layoffs_clean_test
--- WHERE percentage_laid_off < 0.99 AND industry != 'Other'
--- GROUP BY YEAR(date), industry
--- )
+WITH industry_year_breakdown AS (
+SELECT 
+industry,
+COUNT(DISTINCT company) AS num_of_companies,
+CAST(SUM(total_laid_off )AS INT) AS total_laid_off,
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off,
+YEAR(date) AS Year
+FROM layoffs_clean_test
+WHERE percentage_laid_off < 0.99 AND industry != 'Other'
+GROUP BY YEAR(date), industry
+)
 
--- SELECT *
--- FROM industry_year_breakdown
--- WHERE num_of_companies > 5
--- ORDER BY industry, YEAR ASC
+SELECT *
+FROM industry_year_breakdown
+WHERE num_of_companies > 5
+ORDER BY industry, YEAR ASC;
 
 --------------------------------------------------------------------------------------------------------------------
 
@@ -72,31 +74,32 @@
 
 
 --Total and percent of layoffs broken down per country from 2020 through 2026 excluding shutdowns and where the number of companies reporting is greater than 5
--- WITH country_layoffs AS (
--- SELECT
--- country,
--- COUNT(DISTINCT company) AS num_of_companies,
--- CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
--- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
--- FROM layoffs_clean_test
--- WHERE industry != 'Other' AND percentage_laid_off < 0.99
--- GROUP BY country
--- )
+WITH country_layoffs AS (
+SELECT
+country,
+COUNT(DISTINCT company) AS num_of_companies,
+CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
+FROM layoffs_clean_test
+WHERE industry != 'Other' AND percentage_laid_off < 0.99
+GROUP BY country
+)
 
--- SELECT *
--- FROM country_layoffs
--- WHERE num_of_companies > 5
--- WHERE num_of_companies > 5 AND total_laid_off > 10000  --WHERE statment for Substantial Layoffs
+SELECT *
+FROM country_layoffs
+WHERE num_of_companies > 5;
+-- Use query below for Substantial Layoffs findings:
+-- WHERE num_of_companies > 5 AND total_laid_off > 10000
 
 
 --Number of shut down companies per country
--- SELECT
--- country,
--- COUNT(DISTINCT company) AS num_of_companies,
--- CAST(SUM(total_laid_off)AS INT) AS total_laid_off
--- FROM layoffs_clean_test
--- WHERE industry != 'Other' AND percentage_laid_off > 0.99
--- GROUP BY country
+SELECT
+country,
+COUNT(DISTINCT company) AS num_of_companies,
+CAST(SUM(total_laid_off)AS INT) AS total_laid_off
+FROM layoffs_clean_test
+WHERE industry != 'Other' AND percentage_laid_off > 0.99
+GROUP BY country;
 
 
 ----------------------------------------------------------------------------------------------------------------------------------
--- Data_Exploration.sql
+++ Data_Exploration.sql
@@ -1,5 +1,5 @@
 -- table name: layoffs_clean_test
--- source: layoffs_clean.csv
+-- source: data/layoffs_clean.csv
 -- cleaned: pandas
 -- goal: data exploration and investigation
 
@@ -7,53 +7,53 @@
 
 
 --Noticed that Hardware had the 3rd most layoffs at 106k despite only having 38 companies
---Results: A query and found Dell and HP made up 60% with 43k and 23k respectively 
+--Results: A query and found Intel and Dell made up 60% with 43k and 23k respectively 
 
--- SELECT
--- company,
--- CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
--- CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
--- FROM layoffs_clean_test
--- WHERE industry = 'Hardware'
--- GROUP BY company
--- ORDER BY SUM(total_laid_off) DESC
+SELECT
+company,
+CAST(SUM(total_laid_off)AS INT) AS total_laid_off,
+CAST(ROUND(AVG(percentage_laid_off)*100, 2) AS DECIMAL(10, 2)) AS avg_laid_off
+FROM layoffs_clean_test
+WHERE industry = 'Hardware'
+GROUP BY company
+ORDER BY SUM(total_laid_off) DESC;
 
 
 -------------------------------------------------------------------------------------
 
 
 -- Noticed: That when layoff percentages where averaged, some years of industries had a 100 percent 
--- Results: Over the 6 years of the avalibale data, 349 companies had a 100 percent layoff percentage, which almost certianly iducated 349 shutdown comapnies
+-- Results: Over the 6 years of the available data, 349 companies had a 100 percent layoff percentage, which almost certianly indicated 349 shutdown companies
 
--- SELECT 
--- DISTINCT company,
--- percentage_laid_off,
--- industry
--- FROM layoffs_clean_test
--- WHERE percentage_laid_off > 0.99
+SELECT 
+DISTINCT company,
+percentage_laid_off,
+industry
+FROM layoffs_clean_test
+WHERE percentage_laid_off > 0.99;
 
 --------------------------------------------------------------------------------------------------------
 
 --A check to see how many companies shut down per year, 2023 is the clear winner at 105 with the second closet being 2024 at 85
--- SELECT
--- YEAR(date),
--- COUNT(DISTINCT company) AS total_layoffs
--- FROM layoffs_clean_test
--- WHERE percentage_laid_off > 0.99
--- GROUP BY YEAR(date)
--- ORDER BY YEAR(date)
+SELECT
+YEAR(date),
+COUNT(DISTINCT company) AS total_layoffs
+FROM layoffs_clean_test
+WHERE percentage_laid_off > 0.99
+GROUP BY YEAR(date)
+ORDER BY YEAR(date);
 
 
 ----------------------------------------------------------------------------------------------------------
 
 
 --A check to see how the number of shutdown companies per industry per year, became a part of the main analysis
--- SELECT
--- industry,
--- COUNT( DISTINCT company) AS company_count,
--- YEAR(date) AS year
--- FROM layoffs_clean_test
--- WHERE percentage_laid_off > 0.99
--- GROUP BY industry, YEAR(date)
--- ORDER BY industry
+SELECT
+industry,
+COUNT( DISTINCT company) AS company_count,
+YEAR(date) AS year
+FROM layoffs_clean_test
+WHERE percentage_laid_off > 0.99
+GROUP BY industry, YEAR(date)
+ORDER BY industry;
 
